d8eb290db6f69c42935e498e2fc0f73bfe24d994
smielko <smielko@students.kennesaw.edu>
2023-04-29 16:02:15 -0400

    WIP

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         boolean isFinished = false;
+        int [][] Matrix = InitialGraph();
         while (!isFinished) {
             Scanner myScanner = new Scanner(System.in);
             DisplayMenu();
             String selection = myScanner.next();
             switch (selection) {
                 case "1": {
-                    int[][] Matrix = CreateMatrix();
+                    Matrix = CreateMatrix();
                     break;
                 }
                 case "2":
                 {
-                    int[][] Matrix = CreateMatrix();
-                    
+                    OutDegrees(Matrix);
+                    InDegrees(Matrix);
+                    IntegerAdjacencyMatrix(Matrix);
                 }
                     case "3": {
                         isFinished = true;
                     }
-                    default:
-                        System.out.println("Enter valid input");
-
+                    default:System.out.println("Enter valid input");
             }
         }
     }
+    public static int[][] InitialGraph() //clean this up dear god
+    {
+        boolean isFinished = false;
+        while (!isFinished) {
+            Scanner myScanner = new Scanner(System.in);
+            DisplayMenu();
+            String selection = myScanner.next();
+            switch (selection) {
+                case "1":
+                {
+                    int [][]  Matrix = CreateMatrix();
+                    return Matrix;
+                }
+                case "3":
+                {
+                    System.exit(0);
+                }
+                default:
+                    System.out.println("Please enter graph data!");
+            }
+        }
+        return null;
+    }
+
 
     public static int[][] CreateMatrix()
     {
-        int matrixSize = 15;
+        int matrixSize = 15;//arbitrary number
         Scanner myScanner = new Scanner(System.in);
         while (matrixSize > 6)
         {
@@ -41,7 +67,7 @@ public class Main {
         {
             for (int j = 0; j < matrixSize; j++)
             {
-                System.out.printf("Enter [" +i + "] [" + j + "]: ");
+                System.out.printf("Enter A1[" +i + "] [" + j + "]: ");
                 Matrix[i][j] = myScanner.nextInt();
             }
         }
@@ -58,15 +84,13 @@ public class Main {
         for (i = 0; i < G.length; i++)
             for (j = 0; j < G.length; j++)
                 Result[i][j] = G[i][j];
-
         for (k = 0; k < size; k++)
             for (i = 0; i < size; i++)
                 for (j = 0; j < size; j++)
                 {
                     Result[i][j] = Result[i][j] | Result[i][k] & Result[k][j];
                 }
-
-
+        PrintMatrix(Result);
         return Result;
     }
     public static void DisplayMenu()
@@ -92,6 +116,7 @@ public class Main {
                 }
             }
         }
+        PrintMatrix(result);
         return result;
     }
     public static int[][] MatrixAddition(int [][] Matrix1, int [][]Matrix2)
@@ -119,5 +144,65 @@ public class Main {
             System.out.print("\b]\n");
         }
     }
-    //Compute and print out the graph reachability matrix.
+    public static void OutDegrees(int[][] Matrix)
+    {
+        System.out.println("Out-degrees:");
+        int size = Matrix.length;
+        for (int i = 0; i < size; i++)
+        {
+            int sum = 0;
+                for (int j = 0; j < size; j++)
+                {
+                    sum = sum + Matrix[i][j];
+                }
+            System.out.println("Node " + (i+1) +" out-degree is " + sum);
+        }
+    }
+    public static void InDegrees(int[][] Matrix)
+    {
+        System.out.println("In-degrees:");
+        int size = Matrix.length;
+        for (int i = 0; i < size; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < size; j++)
+            {
+                sum = sum + Matrix[j][i];
+            }
+            System.out.println("Node " + (i+1) +" in-degree is " + sum);
+        }
+    }
+    public static void IntegerAdjacencyMatrix(int[][] MatrixA1)
+    {
+        int size = MatrixA1.length, iterator = 0;
+        int[][] MatrixA2 = new int[size][size],
+                MatrixA3 = new int[size][size],
+                MatrixA4 = new int[size][size],
+                MatrixA5 = new int[size][size],
+                IntegerAdjacencyMatrix = new int[size][size];
+
+        if (iterator < size) {
+            MatrixA2 = MatrixMultiplication(MatrixA1, MatrixA1);
+            iterator++;
+            IntegerAdjacencyMatrix = MatrixAddition(MatrixA1,MatrixA2);
+        }
+        if (iterator < size) {
+            MatrixA3 = MatrixMultiplication(MatrixA2, MatrixA1);
+            IntegerAdjacencyMatrix = MatrixAddition(IntegerAdjacencyMatrix,MatrixA3);
+            iterator++;
+        }
+        if (iterator < size) {
+            MatrixA4 = MatrixMultiplication(MatrixA3, MatrixA1);
+            IntegerAdjacencyMatrix = MatrixAddition(IntegerAdjacencyMatrix,MatrixA4);
+            iterator++;
+        }
+        if (iterator < size) {
+            MatrixA5 = MatrixMultiplication(MatrixA4, MatrixA1);
+            IntegerAdjacencyMatrix = MatrixAddition(IntegerAdjacencyMatrix,MatrixA5);
+            iterator++;
+        }
+        System.out.println("Integer Adjacency:");
+        PrintMatrix(IntegerAdjacencyMatrix);
+    }
+
 }
